fdb93135ff55cb585063e8f998c688f7d892e2ea
Joseph Summerhays <summerhays.joseph@gmail.com>
2018-03-18 18:52:00 -0600

    server and script added

--- public/cryptApp.js
+++ public/cryptApp.js
@@ -1,29 +1,110 @@
-var decrypt = (toDecrypt, encryptionKey) => {
+const START_VALID_ASCII_RANGE = 48;
+const LENGTH_VALID_ASCII_RANGE = 79;
+
+var convert = function(toConvert) {
   var toReturn = "";
-  encryptionKey = encryptionKey % 96;
-  for (var i = 0; i < toDecrypt.length; i++) {
-    var c= toDecrypt.charCodeAt(i) - encryptionKey;
-    if (c < 32) {
-      c+= 96;
+  for (var i = 0; i < toConvert.length; i++) {
+    if (toConvert.charAt(i) === " "){
+      toReturn += "_";
     }
-    toReturn += String.fromCharCode(c);
+    else {
+      toReturn += toConvert.charAt(i);
+    }
   }
   return toReturn;
-};
+}
+
+var decrypt = function (toDecrypt, decryptionKey) {
+  console.log("*****************");
+  console.log(toDecrypt+" "+decryptionKey);
+  let toReturn = "";
+  toDecrypt = convert(toDecrypt);
+  let key = Number(decryptionKey);
+  key = key % 96;
+  for (var i = 0; i < toDecrypt.length; i++) {
+    var c = toDecrypt.charCodeAt(i) - key;
+    console.log(c);
+    if (c < START_VALID_ASCII_RANGE) {
+      c+= LENGTH_VALID_ASCII_RANGE;
+    }
+    toReturn += String.fromCharCode(c);
+    console.log(toReturn);
+  }
+  return toReturn;
+}
+
+var encrypt = function (toEncrypt, encryptionKey) {
+  console.log("*****************");
+  console.log(toEncrypt);
+  let key = Number(encryptionKey);
+  toEncrypt = convert(toEncrypt);
+  let toReturn = "";
+  for(var i = 0; i < toEncrypt.length; i++) {
+    let c = toEncrypt.charCodeAt(i);
+    console.log(c);
+    c -= START_VALID_ASCII_RANGE;
+    c += key;
+    c = (c%LENGTH_VALID_ASCII_RANGE)+START_VALID_ASCII_RANGE;
+    toReturn += String.fromCharCode(c);
+    console.log(toReturn);
+  }
+  return toReturn;
+}
+
 
 var app = new Vue({
   el: '#app',
-  data: {},
-  created: () => {
-    setPrimes(7, 13);
-    getPrimes();
-    /*var enc = "encrypted";
-    var key = 1;
-    enc = encrypt(enc, key);
-    console.log(enc);
-    enc = decrypt(enc, key);
-    console.log(enc);*/
+  data: {
+    prime1: 0,
+    prime2: 0,
+    message: "Enter Your message here",
+    crypted: "",
+    decrypted: "",
+    publicKey: 0,
+  },
+  created: function() {
+    //console.log(temp);
+    //console.log(this.prime1);
+
+    axios.get("/api/product").then(response=> {
+      this.publicKey = response.data[0];
+    }).catch(err => {
+    });
+
+    axios.get('/api/message').then(response=> {
+      this.crypted = response.data[0];
+    }).catch(err => {
+    });
+
+  },
+  watch: {
+
   },
   computed: {},
-  methods: {},
+  methods: {
+    setUp: function() {
+      if (this.prime1 > this.prime2) {
+        var key = this.prime1;
+      }
+      else {
+        var key = this.prime2;
+      }
+      var encrypted = encrypt(this.message, key);
+      setPrimes(this.prime1, this.prime2);
+      setMessage(encrypted);
+      this.prime1 = 0;
+      this.prime2 = 0;
+      this.message = "message accepted";
+    },
+    decrypt: function() {
+      if (this.prime1 > this.prime2) {
+        var key = this.prime1;
+      }
+      else {
+        var key = this.prime2;
+      }
+      this.decrypted = decrypt(this.crypted, key);
+
+    }
+  },
 });
--- public/stateManagement.js
+++ public/stateManagement.js
@@ -1,37 +1,30 @@
-var getPublicKey = () => {
-  axios.get("/api/product").then(response=> {
-    console.log("getPrimes");
-    console.log(response);
-  }).catch(err => {
-  });
-};
+/*
+This file was originally designed to also handle the getting,
+however I found it was impossible given my current knowledge
+of javascript.
 
-var getEncryptedMessage = () => {
-  axios.get('/api/message').then(response=> {
-    console.log("get encrypted");
-    console.log(response);
-  }).catch(err => {
-  });
-};
+I needed the getter functions to return the value of the axios.get
+AFTER it got it. but it would asynchronously return before completing the
+call to the server. If I placed the return statement inside the
+.then(function() {}) it would return from the anonymous function rather than
+the overall function.
+
+I don't know any other solutions to this promlem so I just moved the getters
+to inside the cryptApp.js
+
+*/
 
 var setPrimes = (prime1, prime2) => {
   axios.put('/api/prime1/'+prime1).then(response=> {
-    console.log("set first prime");
-    console.log(response);
   }).catch (err=> {
   });
   axios.put('/api/prime2/'+prime2).then(response=> {
-    console.log("set second prime");
-    console.log(response);
   }).catch (err=> {
   });
 };
 
 var setMessage = (message) => {
   axios.put('/api/message/' + message).then(response=> {
-    console.log("setMessage");
-    console.log(response);
-    getEncryptedMessage();
   }).catch(err => {
   });
 }
